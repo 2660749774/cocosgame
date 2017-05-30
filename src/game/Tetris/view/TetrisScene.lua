0d41e75a39088710ca4ff211aaa9bca5cf50a854
2660749774 <2660749774@qq.com>
2017-05-30 18:04:43 +0800

    修复关卡配置bug

--- src/game/Tetris/view/TetrisScene.lua
+++ src/game/Tetris/view/TetrisScene.lua
@@ -92,8 +92,8 @@ function TetrisScene:createPowerView()
 
     local progress = utils.gameArchive:queryData("power.progress")
     if progress then
-        self.currPowerId = progress.powerId
-        self.currArmyId = progress.armyId
+        self.currPowerId = 2 -- progress.powerId
+        self.currArmyId = 24 -- progress.armyId
     else
         self.currPowerId = 1
         self.currArmyId = 1
