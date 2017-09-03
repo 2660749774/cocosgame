d6a28f466bb5fa86a0926618b49a1df47f20c15e
2660749774 <2660749774@qq.com>
2017-09-03 17:05:14 +0800

    打印日志

--- src/main.lua
+++ src/main.lua
@@ -3,12 +3,16 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 
 local platform = cc.Application:getInstance():getTargetPlatform()
 if platform == cc.PLATFORM_OS_ANDROID then
+    print("add search path assets")
     cc.FileUtils:getInstance():addSearchPath("assets/")
 elseif platform == cc.PLATFORM_OS_IPHONE
     or platform == cc.PLATFORM_OS_IPAD then
+    print("add search path Raw")
     cc.FileUtils:getInstance():addSearchPath("Raw/")
 else
     -- mac or win
+    print("add search path src")
+    print("add search path res")
     cc.FileUtils:getInstance():addSearchPath("src/")
     cc.FileUtils:getInstance():addSearchPath("res/")
     cc.FileUtils:getInstance():addSearchPath("../../../res/")
